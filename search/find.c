a1fd5b0b5ca857b559517c58668a1d35e73b5337
devin <qg0624@163.com>
2019-06-15 21:16:23 +0800

    [Author]: devin [Description]: Update the communition protocol and Makefile
    Signed-off-by: devin <qg0624@163.com>

--- search/find.c
+++ search/find.c
@@ -15,10 +15,12 @@
 #include <unistd.h>
 #include <errno.h>
 #include <pthread.h>
+#include "common.h"
 
 #define MAX_RECV_LEN      1024
 #define PUBLIC_PORT       9000
 #define BOARDCAST_IP_ADDR "255.255.255.255"
+
 
 int socket_fd;
 struct sockaddr_in skt_server;
@@ -28,9 +30,10 @@ int quit = 0;
 void *thread_recv()
 {
     char recv[MAX_RECV_LEN] = {0};
-    char device_ip[INET_ADDRSTRLEN] = {0};
     int  socket_len         = 0;
     int  ret                = 0;
+    device_info dev_info    = {0};
+    int  len                = 0;
 
     socket_len = sizeof(skt_device);
 
@@ -39,16 +42,21 @@ void *thread_recv()
         if (ret == -1) {
             perror("can't find any device, wait and try again.\n");
         } else {
-            if (ret >= 4) {
-               if (recv[0] == 0x0A && recv[1] == 0x01 && recv[2] == 0x02) {
-                   inet_ntop(AF_INET, &skt_device.sin_addr, device_ip, sizeof(device_ip));
+            if (ret >= 6) {
+               if (recv[0] == 0x0A && recv[1] == 0x02) {
+                   memset(&dev_info, 0, sizeof(dev_info));
+                   inet_ntop(AF_INET, &skt_device.sin_addr, dev_info.dev_ip, sizeof(dev_info.dev_ip));
                    
-                   printf("device ");
-		   if (recv[3] > 0) {
-		       printf("%c%u ", recv[4], recv[5]);
-                   }
+                   dev_info.dev_type   = recv[2];
+                   dev_info.dev_id     = recv[3];
+                   dev_info.dev_status = (recv[4] == DEVICE_ONLINE) ? DEVICE_ONLINE : DEVICE_OFFLINE;
                    
-                   printf("ip:%s is online.\n", device_ip);
+                   len = (recv[5] >= 32) ? 32 : recv[5];
+                   
+                   memcpy(&dev_info.dev_name, &recv[6], len);
+                   
+                   printf("device(%s:%d): %d", dev_info.dev_name, dev_info.dev_type, dev_info.dev_id);
+                   printf("ip:%s is %s.\n", dev_info.dev_ip, (dev_info.dev_status == DEVICE_ONLINE) ? "ONLINE" : "OFFLINE");
                }
             }
         }
@@ -59,9 +67,8 @@ void *thread_recv()
 
 int main(int argc, char *argv[])
 {
-    char        scan[4]            = {0x0A, 0x01, 0x01, 0x00};
+    char        scan[4]            = {0x0A, 0x01, 0x00, 0x00};
     int         ret                = 0;
-    char        device_ip[INET_ADDRSTRLEN] = {0};
     int         socket_len         = 0;
     int         socket_on          = 1;
     int         socket_br          = 1;
@@ -87,7 +94,7 @@ int main(int argc, char *argv[])
     while(quit != 1)
     {
         sendto(socket_fd, scan, 4, 0, (struct sockaddr *)&skt_server, sizeof(skt_server));
-        sleep(1);
+        sleep(5);
     }
 
 EXIT:
